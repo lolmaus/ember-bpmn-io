d606326bff1f79202c27d9a47dcbe832de0af691
temecom <csmith@temecom.net>
2018-01-19 00:06:45 -0800

    Added Styling

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,18 +1,43 @@
 import Controller from '@ember/controller';
 import BpmnViewer from 'npm:bpmn-js';
+import BpmnModeler from 'npm:bpmn-js/lib/Modeler';
+import Ember from 'ember';
 export default Controller.extend({
-
+	viwer: null,
+	modeler: null, 
 	actions: {
 		render: function() {
-			var viewer = new BpmnViewer({ container: '#canvas' });
-			var bpmn = this.model.bpmn;
+			var viewer = this.get('viewer'); 
+			if (!viewer) {
+				viewer = new BpmnViewer({ container: '#canvas' });
+				this.set('viewer', viewer);
+			}
+			var bpmn = this.model.bpmn.responseText;
 			viewer.importXML(bpmn, function(err) {
 
 				if (!err) {
-					console.log('success!');
+					Ember.Logger.log('success!');
 					viewer.get('canvas').zoom('fit-viewport');
 				} else {
-					console.log('something went wrong:', err);
+					Ember.Logger.log('something went wrong:', err);
+				}
+			});
+		}, 
+		
+		edit: function() {
+			var modeler = this.get('modeler'); 
+			if (!modeler) {
+				modeler = new BpmnModeler({ container: '#canvas' });
+				this.set('modeler', modeler);
+			}
+			var bpmn = this.model.bpmn.responseText;
+			modeler.importXML(bpmn, function(err) {
+
+				if (!err) {
+					Ember.Logger.log('success!');
+					modeler.get('canvas').zoom('fit-viewport');
+				} else {
+					Ember.Logger.log('something went wrong:', err);
 				}
 			});
 		}
