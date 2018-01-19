9f439926901740815253507afec0866578095def
Christopher Smith <christopher.smith8@sdcounty.ca.gov>
2018-01-19 09:33:15 -0800

    Merge with head

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,18 +1,34 @@
 import Controller from '@ember/controller';
 import BpmnViewer from 'npm:bpmn-js';
+import Modeler from 'npm:bpmn-js/lib/Modeler';
+import Ember from 'ember';
 export default Controller.extend({
 
 	actions: {
 		render: function() {
 			var viewer = new BpmnViewer({ container: '#canvas' });
-			var bpmn = this.model.bpmn;
-			viewer.importXML(bpmn, function(err) {
+			var bpmnXML = this.model.bpmn.responseText;
+			viewer.importXML(bpmnXML, function(err) {
 
 				if (!err) {
-					console.log('success!');
+					Ember.Logger.info('success!');
 					viewer.get('canvas').zoom('fit-viewport');
 				} else {
-					console.log('something went wrong:', err);
+					Ember.Logger.error('something went wrong:', err);
+				}
+			});
+		}, 
+		
+		edit: function() {
+			var modeler = new Modeler({ container: '#canvas' });
+			var bpmnXML = this.model.bpmn.responseText;
+			// import diagram
+			modeler.importXML(bpmnXML, function(err) {
+				if (!err) {
+					Ember.Logger.info('BPMN was Loaded succefully!');
+					modeler.get('canvas').zoom('fit-viewport');
+				} else {
+					Ember.Logger.error('something went wrong:', err);
 				}
 			});
 		}
